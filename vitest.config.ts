1e9464ec08f9cdb0a4e60b4fdcb22d505e95e741
KirillBaranov <kirillBaranovJob@yandex.ru>
2025-08-31 01:54:32 +0300

    feat: add minimal coverage

--- vitest.config.ts
+++ vitest.config.ts
@@ -8,5 +8,26 @@ export default defineConfig({
       'packages/**/src/**/*.test.ts',
     ],
     environment: 'node',
+    testTimeout: 20000,
+
+    coverage: {
+      provider: 'v8',
+      all: true,
+      reportsDirectory: './coverage',
+      reporter: ['text', 'json', 'html'],
+      thresholds: {
+        statements: 60,
+        lines: 60,
+        branches: 72,
+        functions: 79,
+      },
+      exclude: [
+        '**/dist/**',
+        '**/fixtures/**',
+        '**/__tests__/**',
+        '**/*.spec.*',
+        '**/*.test.*',
+      ],
+    },
   },
 })
